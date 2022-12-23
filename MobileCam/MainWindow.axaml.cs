838b98e1adf5a049f77f6b1b9378d3c04c69dc51
danii <danii@DESKTOP-ROB8FB8>
2022-12-23 14:38:29 +0300

    Добавьте файлы проекта.

---
+++ MobileCam/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace MobileCam
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
